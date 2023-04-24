e3869ff98aef4f207a532aca0b6e6bfdf55d3c0e
Marc Valdez <marc.valdez@lpunetwork.edu.ph>
2023-04-24 09:09:10 +0800

    added login function added balance check function

--- Automated Teller Machine/main.c
+++ Automated Teller Machine/main.c
@@ -4,64 +4,127 @@
 
 #define INITIAL_BALANCE 5000.00
 
+typedef struct Account {
+    char *name;
+    char *pin_number;
+    float balance;
+    float withdrawal_amount;
+    float deposit;
+} Account;
+
+Account users[] = {
+    {"Valdez, Marc Joshua", "000000", INITIAL_BALANCE, 0.0, 0.0},
+    {"Binegas, John Daniel", "123123", INITIAL_BALANCE, 0.0, 0.0}
+};
+
+Account *account_login();
+void print_menu(Account *user);
 void balance_inquiry();
-void deposit();
+void deposit(float *deposit);
 void withdrawal();
 void change_account();
 
 void main()
 {
-    printf("WELCOME TO LPU BANK\n");
+    Account *user = account_login();
+    
+    printf("\nAccount Name: %s\n", user->name);
+    printf("PIN Number: %s\n", user->pin_number);
+    printf("Initial Balance: %.2f\n", user->balance);
+    printf("Withdrawal Amount: %.2f\n", user->withdrawal_amount);
+    printf("Deposit Amount: %.2f\n\n", user->deposit);
 
-    printf("[1] Balance Inquiry\n");
-    printf("[2] Deposit\n");
-    printf("[3] Withdrawal\n");
-    printf("[4] Logout \\ Change Account\n");
-    printf("[5] Exit\n");
+    print_menu(user);
+}
 
-    int min = 1, max = 5;
-    int in = *(int *)get_number(INTEGER, "\nEnter transaction number. >> ", &min, &max);
-
-    switch(in)
+void print_menu(Account *user)
+{
+    while(1)
     {
-        case 1:
+        printf("WELCOME TO LPU BANK\n\n");
+
+        printf("[1] Balance Inquiry\n");
+        printf("[2] Deposit\n");
+        printf("[3] Withdrawal\n");
+        printf("[4] Logout \\ Change Account\n");
+        printf("[5] Exit\n");
+
+        int min = 1, max = 5;
+        int in = *(int *)get_number(INTEGER, "\nEnter transaction number >> ", &min, &max);
+
+        switch(in)
         {
-            balance_inquiry();
-            break;
+            case 1:
+            {
+                balance_inquiry(user);
+                break;
+            }
+            case 2:
+            {
+                deposit(&users[0].deposit);
+                break;
+            }
+            case 3:
+            {
+                withdrawal();
+                break;
+            }
+            case 4:
+            {
+                change_account();
+                break;
+            }
+            case 5:
+            {
+                printf("\nThank you for banking with us!\n");
+                exit(0);
+                break;
+            }
         }
-        case 2:
-        {
-            deposit();
-            break;
-        }
-        case 3:
-        {
-            withdrawal();
-            break;
-        }
-        case 4:
-        {
-            change_account();
-            break;
-        }
-        case 5:
-        {
-            printf("\nThank you for banking with us!\n");
-            exit(0);
-            break;
-        }
-            
     }
 }
 
-void balance_inquiry()
+Account *account_login()
 {
-    printf("You chose [1]\n");
+    while(1)
+    {
+        char *pin_number = get_text(STRING, "PIN Number >> ", "1234567890");
+        for(int i = 0; i < sizeof(users)/sizeof(users[0]); i++)
+        {
+            if(strcmp(pin_number, users[i].pin_number) == 0)
+            {
+                printf("Pin %d: %s\n", i, users[i].pin_number);
+                return &users[i];
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        printf("Wrong PIN.\n");
+    }
 }
-void deposit()
+
+void balance_inquiry(Account *user)
 {
-    printf("You chose [2]\n");
+    printf("Current Balance: %.2f", user->balance);
 }
+
+void deposit(float *deposit)
+{
+    while(1)
+    {
+        int min = -10000.0, max = 10000.00;
+        deposit = (float *)get_number(FLOAT, "Deposit amount >> ", &min, &max);
+
+        if(*deposit <= 0.0)
+            printf("\n\t* Deposit amount should be greater than zero.\n\n");
+        else
+            break;
+    }
+}
+
 void withdrawal()
 {
     printf("You chose [3]\n");
