d630ce34bcab4aaf3dd5ceabfd45337e004fcfce
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2023-06-15 16:39:10 +0800

    added the rest of the exercises

--- File Processing and Recursion/recursion.c
+++ File Processing and Recursion/recursion.c
@@ -38,6 +38,37 @@ void exercise2()
 	BLAP(4);
 }
 
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
+void exercise3()
+{
+    printf("\nFibonnaci(6) = %d\n", fib(6));
+}
+
+int ackermann(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return ackermann(m - 1, 1);
+    else
+        return ackermann(m - 1, ackermann(m, n - 1));
+}
+
+void exercise4()
+{
+    printf("\nAckermann(0, 10) = %d\n", ackermann(0, 10));
+    printf("Ackermann(1, 2) = %d\n", ackermann(1, 2));
+    printf("Ackermann(2, 1) = %d\n", ackermann(2, 1));
+    printf("Ackermann(3, 0) = %d\n", ackermann(3, 0));
+}
+
 void main()
 {
     // this is just a simple selection menu for the exercise codes above
@@ -47,10 +78,16 @@ void main()
         int choice = 0;
 
         puts("----------------------------------------------\n");
-        for (int i = 1, j = 6; i <= 5, j <= 10; i++, j++)
+        for (int i = 1; i <= 4; i++)
         {
             printf("\tExercise [%d]", i);
-            printf("\tExercise [%d]\n", j);
+			// if(i >= 4)
+			// {
+			// 	printf("\n");
+			// 	break;
+			// }
+			i++;	
+            printf("\tExercise [%d]\n", i);
         }
 
         printf("\nEnter an exercise number [1-10] >> ");
@@ -73,8 +110,8 @@ void main()
         {
             case 1: exercise1(); break;
             case 2: exercise2(); break;
-            // case 3: exercise3(); break;
-            // case 4: exercise4(); break;
+            case 3: exercise3(); break;
+            case 4: exercise4(); break;
             // case 5: exercise5(); break;
             // case 6: exercise6(); break;
             // case 7: exercise7(); break;
