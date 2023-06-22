747945bff42e8b444136f4e560b5e95e2c1e771a
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2023-06-22 09:04:27 +0800

    added another file processing exercise

---
+++ File Processing/main.c
@@ -0,0 +1,29 @@
+#include "valdez_v3.h"
+
+int main()
+{
+    system("cls");
+    
+    // Write a program that will output the characters ‘A’ to ‘Z’ (one character per line) onto a text file whose filename is specified by the user.  
+    // Use fputc() for character output.
+    FILE *outputf;
+    char outputfilename[MAX];
+    
+    get_string(outputfilename, MAX, "> Input name of output text file: ");
+    outputf = fopen(outputfilename, "w");
+    if(outputf == NULL)
+    {
+        printf("ERROR: %s cannot be opened.\n", outputfilename);
+        exit(1);
+    }
+
+    for(char i = 'A'; i <= 'Z'; i++)
+    {
+        fputc(i, outputf);
+        fprintf(outputf, "\n");
+    }
+    
+    fclose(outputf);
+
+    return 0;
+}
---
+++ File Processing/valdez_v3.h
@@ -0,0 +1,301 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+#define MAX 100
+
+/*
+ This header file utilizes ANSI escape codes to color the output.
+ The custom cprintf function takes an INPUT in the form of a TextColor enum.
+ It then takes a string buffer and a variadic argument list. (e.g., %d, %f, %s)
+ It colors the string according to the enum INPUT, then prints it.
+ Lastly, it reverts to the default output color after printing.
+*/
+#define COLOR "\x1B"
+typedef enum TextColor {
+    BLACK = 30,
+    RED = 31,
+    GREEN = 32,
+    YELLOW = 33,
+    BLUE = 34,
+    MAGENTA = 35,
+    CYAN = 36,
+    WHITE = 37,
+    DEFAULT = 0
+} TextColor;
+void cprintf(const TextColor INPUT, const char *buffer, ...)
+{
+    va_list args;
+    va_start(args, buffer);
+
+    printf("%s[%dm", COLOR, INPUT);
+    vprintf(buffer, args);
+    printf("%s[%dm", COLOR, DEFAULT);
+
+    va_end(args);
+}
+
+/*
+ The following functions are used to validate user input.
+ The most common checks are:
+ 1. If the input is empty;
+ 2. If the input contains any whitespace;
+ 3. If the input starts or ends with a period/dot (.)
+*/
+int is_empty(const char *buffer)
+{
+    if(buffer[0] == '\0')
+    {
+        cprintf(RED, "\n! Input cannot be empty. Please enter a value.\n");
+        return 1;
+    }
+    return 0;
+}
+
+int has_whitespace(const char *buffer)
+{
+    if(strchr(buffer, ' ') != NULL)
+    {
+        cprintf(RED, "\n! Invalid input. Please try again without using whitespace characters.\n");
+        return 1;
+    }
+    return 0;
+}
+
+int starts_or_ends_with_dot(const char *buffer)
+{
+    if(buffer[0] == '.' || buffer[strlen(buffer) - 1] == '.')
+    {
+        cprintf(RED, "\n! Invalid input. Please enter a value that does not start or end with a period (.) character.\n");
+        return 1;
+    }
+    return 0;
+}
+
+/*
+ The following functions are used to take user input.
+ Each function takes an address to an integer, float, double, or string to modify.
+ For the numeric functions, a min and max value need to be specified.
+ For the string function, a character limit needs to be specified.
+ All functions require a string literal for the message prompt.
+ It also accepts variadic arguments for the prompt (e.g., %d, %f, %s).
+ There is a preprocessor defined constant called MAX to specify the maximum character length of all inputs.
+*/
+void get_int(int *user_input, const int min, const int max, const char *va_prompt, ...)
+{
+    char prompt[MAX];
+
+    va_list args;
+    va_start(args, va_prompt);
+    vsprintf_s(prompt, strlen(va_prompt) + 1, va_prompt, args);
+    va_end(args);
+
+    char buffer[MAX];
+    while(true)
+    {
+        printf("%s", prompt);
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        if(is_empty(buffer))
+            continue;
+        if(has_whitespace(buffer))
+            continue;
+        if(starts_or_ends_with_dot(buffer))
+            continue;
+
+        if(sscanf_s(buffer, "%d", user_input) != 1)
+        {
+            cprintf(RED, "\n! Invalid input. Please enter a numeric value.\n");
+            continue;
+        }
+
+        char remaining[MAX];
+        if(sscanf_s(buffer, "%f%s", (float *)user_input, remaining) != 1)
+        {
+            cprintf(RED, "\n! Invalid input. Please refrain from using non-numeric characters.\n");
+            continue;
+        }
+
+        if(sscanf_s(buffer, "%d%[.]%s", user_input, remaining, remaining) != 1)
+        {
+            cprintf(YELLOW, "\n! Invalid input. Please enter a whole number.\n");
+            continue;
+        }
+
+        if(*user_input < (int)min || *user_input >(int)max)
+        {
+            cprintf(YELLOW, "\n! Input out of range. Please enter a number between %d and %d (inclusive).\n", (int)min, (int)max);
+            continue;
+        }
+
+        return;
+    }
+}
+
+void get_float(float *user_input, const float min, const float max, const char *va_prompt, ...)
+{
+    char prompt[MAX];
+
+    va_list args;
+    va_start(args, va_prompt);
+    vsprintf_s(prompt, strlen(va_prompt) + 1, va_prompt, args);
+    va_end(args);
+
+    char buffer[MAX];
+    while(true)
+    {
+        printf("%s", prompt);
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        if(is_empty(buffer))
+            continue;
+        if(has_whitespace(buffer))
+            continue;
+        if(starts_or_ends_with_dot(buffer))
+            continue;
+
+        if(sscanf_s(buffer, "%f", user_input) != 1)
+        {
+            cprintf(RED, "\n! Invalid input. Please enter a numeric value.\n");
+            continue;
+        }
+
+        char remaining[MAX];
+        if(sscanf_s(buffer, "%f%s", user_input, remaining) != 1)
+        {
+            cprintf(YELLOW, "\n! Invalid input. Please enter a whole number.\n");
+            continue;
+        }
+
+        if(*user_input < (float)min || *user_input >(float)max)
+        {
+            cprintf(YELLOW, "\n! Input out of range. Please enter a number between %.2f and %.2f (inclusive).\n", (float)min, (float)max);
+            continue;
+        }
+
+        return;
+    }
+}
+
+void get_double(double *user_input, const double min, const double max, const char *va_prompt, ...)
+{
+    char prompt[MAX];
+
+    va_list args;
+    va_start(args, va_prompt);
+    vsprintf_s(prompt, strlen(va_prompt) + 1, va_prompt, args);
+    va_end(args);
+
+    char buffer[MAX];
+    while(true)
+    {
+        printf("%s", prompt);
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        if(is_empty(buffer))
+            continue;
+        if(has_whitespace(buffer))
+            continue;
+        if(starts_or_ends_with_dot(buffer))
+            continue;
+
+        if(sscanf_s(buffer, "%lf", user_input) != 1)
+        {
+            cprintf(RED, "\n! Invalid input. Please enter a numeric value.\n");
+            continue;
+        }
+
+        char remaining[MAX];
+        if(sscanf_s(buffer, "%lf%s", user_input, remaining) != 1)
+        {
+            cprintf(YELLOW, "\n! Invalid input. Please enter a whole number.\n");
+            continue;
+        }
+
+        if(*(double *)user_input < (double)min || *(double *)user_input >(double)max)
+        {
+            cprintf(YELLOW, "\n! Input out of range. Please enter a number between %.2lf and %.2lf (inclusive).\n", (double)min, (double)max);
+            continue;
+        }
+
+        return;
+    }
+}
+
+void get_string(char *user_input, const int limit, const char *va_prompt, ...)
+{
+    char prompt[MAX];
+
+    va_list args;
+    va_start(args, va_prompt);
+    vsprintf_s(prompt, strlen(va_prompt) + 1, va_prompt, args);
+    va_end(args);
+
+    char buffer[MAX];
+    while(true)
+    {
+        printf("%s", prompt);
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        if(is_empty(buffer))
+            continue;
+        if(has_whitespace(buffer))
+            continue;
+        if(starts_or_ends_with_dot(buffer))
+            continue;
+
+        if(sscanf_s(buffer, "%s", user_input) != 1)
+        {
+            cprintf(YELLOW, "\n! Invalid input. Please enter a string of characters.\n");
+            continue;
+        }
+
+        if((int)strlen(user_input) > limit)
+        {
+            cprintf(YELLOW, "\n! Invalid input. Character limit is %d characters.\n", limit);
+            continue;
+        }
+
+        return;
+    }
+}
+
+/*
+ The following function is a simple yes or no question prompt.
+ The user input is not case sensitive.
+ It is up to the programmer to determine what to do next after this function returns either a Yes or No.
+*/
+char yes_or_no(const char *prompt)
+{
+    while(true)
+    {
+        char buffer[MAX];
+        get_string(buffer, 1, prompt);
+
+        if(strspn(buffer, "YyNn") != strlen(buffer))
+        {
+            cprintf(YELLOW, "\n! Invalid input. Please enter Y or N to continue.\n");
+            continue;
+        }
+
+        return buffer[0];
+    }
+}
+
+/*
+ The following function is a simple exit prompt.
+ Instead of exiting abruply, it prints a programmer defined exit message and waits for the user to press any key before exiting.
+*/
+void exit_prompt(const char *prompt)
+{
+    system("cls");
+    printf(prompt);
+    system("pause");
+    exit(0);
+}
